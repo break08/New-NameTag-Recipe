363ee1f85a7d6d5bd6a719e8c941af940b4312c8
break08 <dungluong3108@gmail.com>
2026-04-22 19:17:05 +0700

    First Ver.

---
+++ common/src/main/java/com/example/nntr/Constants.java
@@ -0,0 +1,11 @@
+package com.example.nntr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "new_nametag_recipe";
+	public static final String MOD_NAME = "NewNameTagRecipe";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/com/example/nntr/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package com.example.nntr.platform;
+
+import com.example.nntr.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
