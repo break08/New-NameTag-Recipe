2602dba3f82499d6a03232e6a60dd79ed7bae7cb
break08 <dungluong3108@gmail.com>
2026-04-25 11:13:23 +0700

    Plugin

--- src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
+++ src/main/java/com/crimsonwarpedcraft/exampleplugin/ExamplePlugin.java
@@ -3,45 +3,35 @@ package com.crimsonwarpedcraft.exampleplugin;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.SmithingTransformRecipe;
+import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * NNTR
+ * NNTR.
  *
- * NNTR
+ * <p>NNTR.
  */
 public class ExamplePlugin extends JavaPlugin {
 
   @Override
   public void onEnable() {
-    PaperLib.suggestPaper(this);
+    NamespacedKey key = new NamespacedKey(this, "IronNuggetNT");
+    ItemStack item = new ItemStack(Material.NAME_TAG);
 
-    public class TestPlugin extends JavaPlugin {
+    ShapelessRecipe ironNuggetNntr = new ShapelessRecipe(key, item);
+    ironNuggetNntr.addIngredient(1, Material.PAPER);
+    ironNuggetNntr.addIngredient(1, Material.IRON_NUGGET);
 
-    @Override
-    public void onEnable() {
-        NamespacedKey key = new NamespacedKey(this, "WarriorSword");
-        ItemStack item = ItemStack.of(Material.DIAMOND_SWORD);
+    getServer().addRecipe(ironNuggetNntr);
 
-        ShapelessRecipe iron_nugget_nntr = new ShapelessRecipe(key, item);
-        iron_nugget_nntr.addIngredient(1, Material.PAPER);
-        iron_nugget_nntr.addIngredient(1, Material.IRON_NUGGET);
+    NamespacedKey keyGold = new NamespacedKey(this, "GoldNuggetNT");
+    ItemStack itemGold = new ItemStack(Material.NAME_TAG);
 
-        getServer().addRecipe(iron_nugget_nntr);
+    ShapelessRecipe goldNuggetNntr = new ShapelessRecipe(keyGold, itemGold);
+    goldNuggetNntr.addIngredient(1, Material.PAPER);
+    goldNuggetNntr.addIngredient(1, Material.GOLD_NUGGET);
 
-        NamespacedKey key_gold = new NamespacedKey(this, "WarriorSword");
-        ItemStack item_gold = ItemStack.of(Material.DIAMOND_SWORD);
-
-        ShapelessRecipe gold_nugget_nntr = new ShapelessRecipe(key_gold, item_gold);
-        gold_nugget_nntr.addIngredient(1, Material.PAPER);
-        gold_nugget_nntr.addIngredient(1, Material.GOLD_NUGGET);
-
-        getServer().addRecipe(gold_nugget_nntr);
-    }
-}
-
+    getServer().addRecipe(goldNuggetNntr);
     saveDefaultConfig();
   }
 }
